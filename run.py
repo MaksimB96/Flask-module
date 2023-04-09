ee0c3df6dc05fe027adad368347e052a1ed37ef8
MaksimB96 <>
2023-04-09 17:54:26 +0000

    Add all corrrect links etc. from follow along

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -28,5 +29,4 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
-    )
+        debug=True)
